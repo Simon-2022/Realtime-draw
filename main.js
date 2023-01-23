f341044b1cb25ee172475a6dfa75f5b58604cae4
Simon-2022 <96212730+Simon-2022@users.noreply.github.com>
2023-01-23 19:00:50 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var noseX = 0;
+var noseY = 0;
+var difference = 0;
+var rightWristX = 0;
+var leftWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -12,6 +18,17 @@ function setup() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
+        console.log("noseX = " + noseX + " noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
     }
 }
 
@@ -21,4 +38,8 @@ function modelLoaded() {
 
 function draw() {
     background('#969A97');
+    document.getElementById("square_sides").innerHTML = "Width And Height of the Square will be = " + difference + "px";
+    fill('blue');
+    stroke('red');
+    square(noseX, noseY, difference)
 }
